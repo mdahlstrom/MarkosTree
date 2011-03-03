18eb417d9945607b6b6143735a18cfcd18d89ccf
Mathias Dahlstrom <mathias.dahlstrom@lastminute.com>
2011-03-03 16:21:34 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarkosTree::Application.config.secret_token = 'b97ff150cd170fa876089893927119f4fd7f9143dd6b896a2d61e12470973762bc5665c04d145857bab708600396b9ae37d67d06b3fc7ec63b2bdfcb1041ebd1'
