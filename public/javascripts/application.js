490817c226ae9b1fa96886b30b96d4b8726580ab
Mathias Dahlstrom <mathias.dahlstrom@gmail.com>
2011-03-04 18:31:54 +0000

    added animation for leaving people

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	setTimeout( jasonFall, 4000);
+	setTimeout( samFall, 8000);
+	setTimeout( kimFall, 11000);
+	setTimeout( markoFall, 12400);
+});
+
+
+function jasonFall() {
+	$("img.jason").removeClass("wiggle_reverse");
+	$("img.jason").removeClass("wiggle");
+	$("img.jason").addClass("jason_drop");
+	$("img.jason").css("bottom","30px")
+	$("img.jason").css("left","200px");
+}
+
+function samFall() {
+	$("img.sam").removeClass("wiggle_reverse");
+	$("img.sam").removeClass("wiggle");
+	$("img.sam").addClass("sam_drop");
+	$("img.sam").css("bottom","30px")
+	$("img.sam").css("left","35px");
+}
+
+function kimFall() {
+	$("img.kim").removeClass("wiggle_reverse");
+	$("img.kim").removeClass("wiggle");
+	$("img.kim").addClass("kim_drop");
+	$("img.kim").css("bottom","30px")
+	$("img.kim").css("left","243px");
+}
+
+function markoFall() {
+	$("img.marko").removeClass("wiggle_reverse");
+	$("img.marko").removeClass("wiggle");
+	$("img.marko").addClass("marko_drop");
+	$("img.marko").css("bottom","30px")
+	$("img.marko").css("left","130px");
+}
